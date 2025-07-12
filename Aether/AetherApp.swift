70577ef1b40426392ecdcd2e4bf80b30efe5aad1
nukehead1 <deepakpatnaik1@gmail.com>
2025-07-12 09:28:05 +0200

    Initial Commit

---
+++ Aether/AetherApp.swift
@@ -0,0 +1,17 @@
+//
+//  AetherApp.swift
+//  Aether
+//
+//  Created by Deepak Patnaik on 12.07.25.
+//
+
+import SwiftUI
+
+@main
+struct AetherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
